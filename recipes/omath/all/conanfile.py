c3de3ab5be8ac6f27787bc7b3c42f215430013ce
Saikari <lin@sz.cn.eu.org>
2026-09-07 13:57:51 +0300

    Fix modules

--- recipes/omath/all/conanfile.py
+++ recipes/omath/all/conanfile.py
@@ -92,7 +92,8 @@ class OmathConan(ConanFile):
         cmake = CMake(self)
         cmake.install()
 
-        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        if not self.options.get_safe("enable_modules"):
+            rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "omath")
